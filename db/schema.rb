a9ffb69afde32ff202103dcffd156c39354d70a7
rob craig <heidlessemail08@gmail.com>
2024-08-29 15:44:29 +0100

    added post functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_27_165910) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_28_154501) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 end
